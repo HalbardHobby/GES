bc35d64d20f0d02079d005c7d0591008005394d3
Alejandro Espinosa <HalbardHobby@users.noreply.github.com>
2019-08-25 21:02:31 -0500

    naming refactor

--- mos6502/instructionSet.go
+++ mos6502/instructionSet.go
@@ -1,9 +1,228 @@
 package mos6502
 
-type instruction struct {
-	mode   func() // Addressing mode for the function
-	opcode func() // opcode to execute
-	size   byte   // Instruction size
-	cycles byte   // Number of cycles used by the instruction
-	name   string // instruction name
+// OPCODES IMPLEMENTATION //
+
+func (cpu *CPU) adc(func(*CPU) uint8) {
+
+}
+
+func (cpu *CPU) and() {
+
+}
+
+func (cpu *CPU) asl() {
+
+}
+
+func (cpu *CPU) bcc() {
+
+}
+
+func (cpu *CPU) bcs() {
+
+}
+
+func (cpu *CPU) beq() {
+
+}
+
+func (cpu *CPU) bit() {
+
+}
+
+func (cpu *CPU) bmi() {
+
+}
+
+func (cpu *CPU) bne() {
+
+}
+
+func (cpu *CPU) bpl() {
+
+}
+
+func (cpu *CPU) brk() {
+
+}
+
+func (cpu *CPU) bvc() {
+
+}
+
+func (cpu *CPU) bvs() {
+
+}
+
+func (cpu *CPU) clc() {
+	cpu.processorStatus.clear(carry)
+}
+
+func (cpu *CPU) cld() {
+	cpu.processorStatus.clear(decimal)
+}
+
+func (cpu *CPU) cli() {
+	cpu.processorStatus.clear(interruptDisable)
+}
+
+func (cpu *CPU) clv() {
+	cpu.processorStatus.clear(overflow)
+}
+
+func (cpu *CPU) cmp() {
+
+}
+
+func (cpu *CPU) cpx() {
+
+}
+
+func (cpu *CPU) cpy() {
+
+}
+
+func (cpu *CPU) dec() {
+
+}
+
+func (cpu *CPU) dex() {
+
+}
+
+func (cpu *CPU) dey() {
+
+}
+
+func (cpu *CPU) eor() {
+
+}
+
+func (cpu *CPU) inc() {
+
+}
+
+func (cpu *CPU) inx() {
+
+}
+
+func (cpu *CPU) iny() {
+
+}
+
+func (cpu *CPU) jmp() {
+
+}
+
+func (cpu *CPU) jsr() {
+
+}
+
+func (cpu *CPU) lda() {
+
+}
+
+func (cpu *CPU) ldx() {
+
+}
+
+func (cpu *CPU) ldy() {
+
+}
+
+func (cpu *CPU) lsr() {
+
+}
+
+func (cpu *CPU) nop() {
+	return
+}
+
+func (cpu *CPU) ora() {
+
+}
+
+func (cpu *CPU) pha() {
+
+}
+
+func (cpu *CPU) php() {
+
+}
+
+func (cpu *CPU) pla() {
+
+}
+
+func (cpu *CPU) plp() {
+
+}
+
+func (cpu *CPU) rol() {
+
+}
+
+func (cpu *CPU) ror() {
+
+}
+
+func (cpu *CPU) rti() {
+
+}
+
+func (cpu *CPU) rts() {
+
+}
+
+func (cpu *CPU) sbc() {
+
+}
+
+func (cpu *CPU) sec() {
+
+}
+
+func (cpu *CPU) sed() {
+
+}
+
+func (cpu *CPU) sei() {
+
+}
+
+func (cpu *CPU) sta() {
+
+}
+
+func (cpu *CPU) stx() {
+
+}
+
+func (cpu *CPU) sty() {
+
+}
+
+func (cpu *CPU) tax() {
+	cpu.indexX = cpu.accumulator
+	// TODO set flags
+}
+
+func (cpu *CPU) tay() {
+
+}
+
+func (cpu *CPU) tsx() {
+
+}
+
+func (cpu *CPU) txa() {
+
+}
+
+func (cpu *CPU) txs() {
+
+}
+
+func (cpu *CPU) tya() {
+
 }
